3dd567a2db8586e8417ed815e64e3b62f16ee985
Cameron Payne <cmpayne@yahoo.com>
2014-03-09 16:24:01 -0400

    Herokued

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Pinteresting::Application.routes.draw do
  root "pages#home"
  get "about" => "pages#about"
+ get "knuck" => "pages#knuck"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
